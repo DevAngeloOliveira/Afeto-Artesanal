c6028cd606d993f2a448b50de212969ffb0e1a61
DevAngeloOliveira <dev.angelooliveira@gmail.com>
2024-12-16 16:09:38 -0300

    fix: atualizando dependencias e configurações para o deploy

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    domains: ['localhost'],
+    unoptimized: true,
   },
+  swcMinify: true,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig 
